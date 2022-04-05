a96b549ecfc8a78274a16e4d1bed9d1b836f6228
JosephShin <joespam9999@gmail.com>
2022-04-05 16:16:43 -0400

    commit tree d6120882c4f3ca8c56dc44f8d00c754f829e050f

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(names,event){
+    const message = [];
+    for(let i=0; i < names.length; i++){
+        message[i] = `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+    }
+    return message
+}
+
+function countDown(num){
+    while(num >= 0){
+        console.log(num)
+        num--;
+    }
+}
